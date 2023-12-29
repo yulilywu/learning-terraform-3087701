98f9e8931f3af749724b30d032eac962297a92e8
yulilywu <43283169+yulilywu@users.noreply.github.com>
2023-12-29 08:40:29 -0800

    Add target_id to blog-instance

--- main.tf
+++ main.tf
@@ -46,20 +46,22 @@ resource "aws_instance" "blog" {
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
+  version = "9.4.0"
 
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  load_balancer_type = "application"  
-
+  
   target_groups = {
     blog-instance = {
-      name_prefix      = "blog-"
+      name_prefix      = "blog"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+
+      target_id        = aws_instance.blog.id
     }
   }
 
