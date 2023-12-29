bc42ad8390f83fb5d0d55e5301879a14c2fc01eb
yulilywu <43283169+yulilywu@users.noreply.github.com>
2023-12-28 16:48:33 -0800

    load balancer

--- main.tf
+++ main.tf
@@ -44,6 +44,39 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  load_balancer_type = "application"  
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
